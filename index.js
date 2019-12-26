83da3cc5cfb2d600feb51c826b909d368e0aead1
GaloyanLilit <galoyan95lilit@mail.ru>
2019-12-26 21:39:54 +0400

    new home-work

--- index.js
+++ index.js
@@ -1,57 +1,192 @@
- const fs = require('fs');
- const arg = require('minimist')(process.argv.slice(2));
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
+// const {e, fileName} = require('minimist')(process.argv.slice(2));
+
+// function getFileContent(fileName, ext, res){
+//     fs.readFile("./main.html", "utf-8", (err, data) => {
+//       let wdata = data;
+//       let statusCode = 200;
+//       if(err){
+//         wdata = "File not found";
+//         statusCode = 200
+        
+      
+//       }
+//       res.writeHead(statusCode, {
+//         "Content-Type":"text/html;charset=utf-8"
+//       });
+
+    
+//         res.write(data);
+//         res.end();
+      
+//     });
+// }
+
+// http.createServer((req, res) =>{
+//   res.writeHead(200, {
+//     "Content-Type":"text/html;charset=utf-8"
+//   });
+//   ///sa browseri hamar e format cuyc talis,, verevi utf-8@ backendi hamar, mez hamar
+//   //header mek angam e petq uxarkel,, dra hamar errorneri obrabotka aneluc sxal e stacvum
+
+//   if(e && fileName){
+//     getFileContent(fileName, e, res);
+    
+//   } else{
+//     res.write("Hello, World");
+//     res.end()
+//   }
+// }).listen(3000)
 
 
-// function getFileContent(fileName){
-//   fs.readFile(`./${fileName}`,'utf-8',(err, data) => {
-//     if(err){
-//       console.log(`${fileName}.txt not found`);
-//     }else{
-//       console.log(data);
-//     }
+// const http = require('http');
+// const server = http.createServer().listen(8080);
+
+// server.on('request',(req,res) =>{
+//    res.writeHead(200,{
+//       "Content-Type":"text/html;charset=utf-8"
+//    });
+//    res.write('Hello');
+//    res.end();
 // });
-// }
+
+// server.on('listening',() =>{
+//    console.log('Listening on port 8080');
+// });
+
+// const http = require('http');
+// const serverPort = 3000;
+// const server = http.createServer().listen(serverPort);
+
+// server.on('request', (req,res)=>{
+//    res.writeHead(200);
+//    res.write('barev dzez kareli e canotanal???');
+//    res.end();
+// });
+
+// server.on('request', (req,res)=>{
+//   console.log(`Request url: ${req.url}`);
+//   console.log(`Request method: ${req.method}`);
+// });
 
 
-// switch(arg.f || arg.file || arg.c || arg.css){
-//     case arg.f:getFileContent(`${arg.f}.txt`)
-//     break;
-//     case arg.file: getFileContent(`${arg.file}.txt`)
-//     break;
-//     case arg.c:getFileContent(`${arg.c}.css`)
-//     break;
-//     case arg.css: getFileContent(`${arg.css}.css`)
-//     break;
-// }
+// server.on('listening',()=>{
+//    console.log(`Server listen ${serverPort} port`);
+// });
+
+
+// const {EventEmitter} = require ('events');
+
+// const testObj = new EventEmitter();
+
+// testObj.on('hello', () =>{
+//    console.log('LLLLLLLLLLLLLLLLLLLLLLLLLLLLLL');
+// });
+
+
+// testObj.on('world', () =>{
+//    console.log('TTTTTTTTTTTTTTTTTTTTTTTTTTTTT');
+// });
+
+// testObj.emit('hello');
+// testObj.emit('world')
 
 
 
-http.createServer((req, res) =>{
-  res.writeHead(200,{
-      'Content-Type': 'text/html'
-  });
+// const {EventEmitter} = require ('events');
 
-  function getFileContent(fileName){
-    fs.readFile(`./${fileName}`,'utf-8',(err, data) => {
-      if(err){
-        console.log(`${fileName} not found`);
-      }else{
-        console.log(data);
-      }
-  });
-  }
-  
-  
-  switch(arg.e || arg.file){
-      case 'hello': getFileContent(`${arg.fileName}.txt`)
-      break;
-      case 'main':getFileContent(`${arg.fileName}.html`)
-      break;
-      
-      
-  }
-  res.end();
+// const testObj = new EventEmitter();
 
-}).listen(8080)
+// testObj.on('hello', ({x,y}) =>{
+//    console.log(x,y);
+// });
 
+
+// testObj.on('world', (a,b) =>{
+//    console.log(a,b);
+// });
+
+// testObj.emit('hello',{
+//    x:25,
+//    y:15
+// });
+// testObj.emit('world',55,35)
+
+// const {EventEmitter} = require ('events');
+
+// const logger = new EventEmitter();
+
+// const users =[];
+// const msgs = [];
+
+// logger.on('message',(msg)=>{
+//    console.log(`New message: ${msg}`);
+//    msgs.push(msg);
+// });
+
+// logger.on('login',(name)=>{
+//    console.log(`New user: ${name}`);
+//    users.push(name);
+// });
+
+// logger.on('getUsers',()=>{
+//    console.log(`Logged users:\n`);
+//    users.forEach((el) =>{
+//       console.log(el, '\n')
+//    });
+// });
+
+// logger.on('getMessages',()=>{
+//    console.log(`Logged messages:\n`);
+//    msgs.forEach((el) =>{
+//       console.log(el, '\n')
+//    });
+// });
+
+// logger.emit('login','Lilit');
+// logger.emit('message','Barev');
+
+
+// logger.emit('login','Mane');
+// logger.emit('message','Privet');
+
+// logger.emit('login','Ani');
+// logger.emit('message','Voxjuyn');
+
+// logger.emit('getUsers');
+// logger.emit('getMessages');
+
+
+// const fs = require('fs');
+
+// fs.readdir('../myapp1',(err,files) =>{
+//    console.log(files);
+// });
+
+// fs.stat('../myapp1/package.json',(err,obj)=>{
+//    console.log(obj.isFile());
+//    console.log(obj.isDirectory());
+// })
+
+
+const fs = require('fs');
+
+fs.readdir('../node',(err,files) =>{
+      console.log(files);
+   });
+fs.readdir('../node/node_modules/minimist',(err,files)=>{
+   console.log(files)
+})
+fs.readdir('../node/node_modules',(err,files)=>{
+   console.log(files)
+})
+
+   // fs.readdir('../po/scss',(err,files)=>{
+   //    console.log(files);
+   // })
+
+   fs.stat('../node/node_modules',(err,obj)=>{
+      console.log(obj.isFile());
+      console.log(obj.isDirectory())
+   })
