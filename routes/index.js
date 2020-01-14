9a448443193fdf95744cb5f34c6a65fa791325a1
GaloyanLilit <galoyan95lilit@mail.ru>
2020-01-14 22:42:33 +0400

    homew

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('home',{
+
+    name:'John'
+  });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+module.exports = router;
